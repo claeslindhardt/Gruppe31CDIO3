08f77e5bc7ff6308f7126e80b11b3aea7eab326f
claeslindhardt <30341730+claeslindhardt@users.noreply.github.com>
2018-11-14 20:37:32 +0100

    oprettet pakker der sortere data og kontroller klasser mere gennemførligt

--- src/main/java/navneGenerering/controllerKlasser/JernbaneDoeber.java
+++ src/main/java/navneGenerering/controllerKlasser/JernbaneDoeber.java
@@ -3,4 +3,14 @@ package navneGenerering.controllerKlasser;
 import navneGenerering.DataKlasser.NavneGenerator;
 
 public class JernbaneDoeber extends NavneGenerator {
+    int vejnavn = getRand().nextInt(getVejNavn().length);
+    public JernbaneDoeber(){
+        setGeneretNavn(getVejNavn()[vejnavn].concat(" Station"));
+        for(int i = 0; i<getAlleNavne().size(); i++){
+            if(getGeneretNavn() == getAlleNavne().get(i)){
+                setGeneretNavn(getVejNavn()[vejnavn].concat(" 2'nd ").concat(" Station"));
+            }
+        }
+        addAlleNavne(getGeneretNavn());
+    }
 }
