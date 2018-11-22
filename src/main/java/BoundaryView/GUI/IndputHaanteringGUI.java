1a23b747cd4dddc269e4bc46c934db3943977021
claeslindhardt <30341730+claeslindhardt@users.noreply.github.com>
2018-11-22 11:42:40 +0100

    made improvement to GUIinterface

---
+++ src/main/java/BoundaryView/GUI/IndputHaanteringGUI.java
@@ -0,0 +1,38 @@
+package BoundaryView.GUI;
+
+public class IndputHaanteringGUI {
+    public int velkomstMenu(String valg){
+        int menuOpt = 1;
+        if(valg == "starte nyt spil"){
+            menuOpt = 1;
+        }else if(valg == "aendre spil instillinger"){
+            menuOpt = 2;
+        }else if(valg == "forsaette sidste spil"){
+            menuOpt = 3;
+        }
+        return menuOpt;
+    }
+    public int TurMenu(String valg){
+        int beslutning = 0;
+        if(valg == "Kast terninger"){
+            beslutning = 1;
+        }else if(valg == "Slut din tur"){
+            beslutning = 2;
+        }else if(valg == "Se chancekort"){
+            beslutning = 3;
+        }else if(valg == "Se hvad du ejer"){
+            beslutning = 2;
+        }else if(valg == "Se spiller stats"){
+            beslutning = 3;
+        }else if(valg == "Giv op"){
+            beslutning = 2;
+        }else if(valg == "Byg på ejendom"){
+            beslutning = 3;
+        }else if(valg == "Handel med Ejede ting"){
+            beslutning = 3;
+        }
+        return beslutning;
+    }
+
+
+}
