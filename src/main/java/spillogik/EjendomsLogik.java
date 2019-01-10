6936d6fe6392eb6247d70e4d506b0addb15bf662
maltebp <maltebp94@gmail.com>
2019-01-10 11:59:41 +0100

    EjendomsLogik: tilfoejet leje beregnings metoder

--- src/main/java/spillogik/EjendomsLogik.java
+++ src/main/java/spillogik/EjendomsLogik.java
@@ -8,8 +8,45 @@ public class EjendomsLogik {
     /** Private constructor sikrer at man ikke kan lave objekter af klassen. */
     private EjendomsLogik(){}
 
-    public static int beregnLeje(EjendomCO ejendom){
-        return 0;
+
+
+    public boolean kanKoebeHotel(){
+        return false;
+    }
+
+
+    public boolean kanKoebeHus(){
+        return false;
+    }
+
+
+    public static int beregnLeje( EjendomCO ejendom, boolean harAlleIGruppe ){
+        int leje;
+
+        if( ejendom.getAntalHuse() > 0 ){
+            leje = ejendom.getLejeHus(ejendom.getAntalHuse());
+
+        }else if( ejendom.harHotel() ){
+            leje = ejendom.getLejeHotel();
+
+        }else{
+            leje = ejendom.getLejeStart();
+
+            if( harAlleIGruppe ){
+                leje *= 2;
+            }
+        }
+
+        return leje;
+    }
+
+
+
+    public static int beregnLejeVedHus(EjendomCO ejendom, int antalHuse){
+
+        int lejePerHus = ejendom.getLejeStart() * 2;
+
+        return lejePerHus * antalHuse;
     }
 
 
