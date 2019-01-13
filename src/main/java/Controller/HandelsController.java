0e10a00d1ed9c7e0ca08f87f9f0e63ab82683a3a
Andreas <s130016@student.dtu.dk>
2019-01-13 11:08:07 +0100

    Javadoc- på indsamleLeje-metode i HandelsControllerKLasse

--- src/main/java/Controller/HandelsController.java
+++ src/main/java/Controller/HandelsController.java
@@ -4,10 +4,21 @@ import BoundaryView.UserInterfaceKontrakt;
 import ModelEnteties.felter.EjendomCO;
 import spillogik.EjendomsLogik;
 
+/*Denne klasse er en subcontroller.
+* Denne controller håndtere alle alle metoder der har med pengetransaktioner at gøre.
+* */
+
+
 public class HandelsController {
 
 
-
+    /**
+     * @author Andreas
+     * Metoder der indsamlere leje for når man lander på et ejeligt felt.
+     * @param ejendom
+     * @param spilleren
+     * @param userInterfaceKontrakt
+     */
     public void indsamleLeje(EjendomCO ejendom,SpillerCO spilleren, UserInterfaceKontrakt userInterfaceKontrakt){
         SpillerCO ejeren = ejendom.getEjer();
         if( ejeren != null && spilleren != null) {
