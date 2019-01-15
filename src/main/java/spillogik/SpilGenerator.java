052d004681f6792b1fb04981865497d462af6881
JRJ86 <42966158+JRJ86@users.noreply.github.com>
2019-01-15 16:09:37 +0100

    Igang med at generere original bræt - 6th commit

--- src/main/java/spillogik/SpilGenerator.java
+++ src/main/java/spillogik/SpilGenerator.java
@@ -8,6 +8,7 @@ import ModelEnteties.Spiller;
 import ModelEnteties.felter.*;
 import ModelEnteties.raflebaeger.RafleBaeger;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class SpilGenerator {
@@ -91,9 +92,13 @@ public class SpilGenerator {
         FaengselCO faengsel;
         GaaIFaengselCO gaaIFaengsel;
         FriParkering friParkering;
+        EjendomsGruppeDTO ejendomsGruppe;
+
 
     //----------------------------------------Brættet---------------------------------------//
         felter[0] = new StartCO(200,0);
+
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.blue,2);
 
         ejendom = new EjendomCO("Rødovrevej",60,2,1);
         ejendom.setLejeHus(10,30,90,160);
@@ -101,6 +106,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(30);
         ejendom.setHusPris(50);
         ejendom.setHotelPris(50);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[1] = ejendom;
 
         proevLykken = new ChanceFeltCO(2,null);
@@ -112,6 +118,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(30);
         ejendom.setHusPris(50);
         ejendom.setHotelPris(50);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[3] = ejendom;
 
         indkomstSkat = new BetalSkat();
@@ -122,12 +129,15 @@ public class SpilGenerator {
         rederi.setPantsaetningsVaerdi(100);
         felter[5] = rederi;
 
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.PINK,3);
+
         ejendom = new EjendomCO("Roskildevej",100,6,6);
         ejendom.setLejeHus(30,90,270,400);
         ejendom.setLejeHotel(550);
         ejendom.setPantsaetningsVaerdi(50);
         ejendom.setHusPris(50);
         ejendom.setHotelPris(50);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[6] = ejendom;
 
         proevLykken = new ChanceFeltCO(7,null);
@@ -139,6 +149,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(50);
         ejendom.setHusPris(50);
         ejendom.setHotelPris(50);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[8] = ejendom;
 
         ejendom = new EjendomCO("Allégade",120,8,9);
@@ -147,10 +158,13 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(60);
         ejendom.setHusPris(50);
         ejendom.setHotelPris(50);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[9] = ejendom;
 
         faengsel = new FaengselCO("Fængsel",10);
         felter[10] = faengsel;
+
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.green,3);
 
         ejendom = new EjendomCO("Frederiksberg Allé",140,10,11);
         ejendom.setLejeHus(50,150,450,625);
@@ -158,6 +172,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(70);
         ejendom.setHusPris(100);
         ejendom.setHotelPris(100);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[11] = ejendom;
 
         bryggeri = new Bryggeri("Tuborg",12);
@@ -171,6 +186,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(70);
         ejendom.setHusPris(100);
         ejendom.setHotelPris(100);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[13] = ejendom;
 
         ejendom = new EjendomCO("Gl. Kongevej",160,12,14);
@@ -179,6 +195,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(80);
         ejendom.setHusPris(100);
         ejendom.setHotelPris(100);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[14] = ejendom;
 
         rederi = new Rederi("D.F.D.S",200,15);
@@ -186,12 +203,15 @@ public class SpilGenerator {
         rederi.setPantsaetningsVaerdi(100);
         felter[15] = rederi;
 
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.GRAY,3);
+
         ejendom = new EjendomCO("Bernstorffsvej",180,14,16);
         ejendom.setLejeHus(70,200,550,750);
         ejendom.setLejeHotel(950);
         ejendom.setPantsaetningsVaerdi(90);
         ejendom.setHusPris(100);
         ejendom.setHotelPris(100);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[16] = ejendom;
 
         proevLykken = new ChanceFeltCO(17,null);
@@ -203,6 +223,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(90);
         ejendom.setHusPris(100);
         ejendom.setHotelPris(100);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[18] = ejendom;
 
         ejendom = new EjendomCO("Strandvej",200,16,19);
@@ -211,10 +232,13 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(100);
         ejendom.setHusPris(100);
         ejendom.setHotelPris(100);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[19] = ejendom;
 
         friParkering = new FriParkering();
         felter[20] = friParkering;
+
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.red,3);
 
         ejendom = new EjendomCO("Trianglen",220,18,21);
         ejendom.setLejeHus(90,250,700,875);
@@ -222,6 +246,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(110);
         ejendom.setHusPris(150);
         ejendom.setHotelPris(150);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[21] = ejendom;
 
         proevLykken = new ChanceFeltCO(22,null);
@@ -233,6 +258,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(110);
         ejendom.setHusPris(150);
         ejendom.setHotelPris(150);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[23] = ejendom;
 
         ejendom = new EjendomCO("Grønningen",240,20,24);
@@ -241,6 +267,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(120);
         ejendom.setHusPris(150);
         ejendom.setHotelPris(150);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[24] = ejendom;
 
         rederi = new Rederi("Ø.K",200,25);
@@ -248,12 +275,15 @@ public class SpilGenerator {
         rederi.setPantsaetningsVaerdi(100);
         felter[25] = rederi;
 
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.white,3);
+
         ejendom = new EjendomCO("Bredgade",260,22,26);
         ejendom.setLejeHus(110,330,800,975);
         ejendom.setLejeHotel(1150);
         ejendom.setPantsaetningsVaerdi(130);
         ejendom.setHusPris(150);
         ejendom.setHotelPris(150);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[26] = ejendom;
 
         ejendom = new EjendomCO("Kgs. Nytorv",260,22,27);
@@ -262,6 +292,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(130);
         ejendom.setHusPris(150);
         ejendom.setHotelPris(150);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[27] = ejendom;
 
         bryggeri = new Bryggeri("Carlsberg",28);
@@ -275,10 +306,13 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(140);
         ejendom.setHusPris(150);
         ejendom.setHotelPris(150);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[29] = ejendom;
 
         gaaIFaengsel = new GaaIFaengselCO(30);
         felter[30] = gaaIFaengsel;
+
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.YELLOW,3);
 
         ejendom = new EjendomCO("Amagertorv",300,26,31);
         ejendom.setLejeHus(130,390,900,1100);
@@ -286,6 +320,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(150);
         ejendom.setHusPris(200);
         ejendom.setHotelPris(200);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[31] = ejendom;
 
         ejendom = new EjendomCO("Vimmelskaftet",300,26,32);
@@ -294,6 +329,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(150);
         ejendom.setHusPris(200);
         ejendom.setHotelPris(200);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[32] = ejendom;
 
         proevLykken = new ChanceFeltCO(33,null);
@@ -305,6 +341,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(160);
         ejendom.setHusPris(200);
         ejendom.setHotelPris(200);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[34] = ejendom;
 
         rederi = new Rederi("Bornholm 1866",200,35);
@@ -315,12 +352,15 @@ public class SpilGenerator {
         proevLykken = new ChanceFeltCO(36,null);
         felter[36] = proevLykken;
 
+        ejendomsGruppe = new EjendomsGruppeDTO(Color.MAGENTA,2);
+
         ejendom = new EjendomCO("Frederiksberggade",350,35,37);
         ejendom.setLejeHus(175,500,1100,1300);
         ejendom.setLejeHotel(1500);
         ejendom.setPantsaetningsVaerdi(175);
         ejendom.setHusPris(200);
         ejendom.setHotelPris(200);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[37] = ejendom;
 
         statsSkat = new BetalSkat();
@@ -332,6 +372,7 @@ public class SpilGenerator {
         ejendom.setPantsaetningsVaerdi(200);
         ejendom.setHusPris(200);
         ejendom.setHotelPris(200);
+        ejendomsGruppe.tilfoejEjendom(ejendom);
         felter[39] = ejendom;
 
     }
