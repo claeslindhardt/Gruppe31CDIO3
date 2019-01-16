7c096995a0c245c04fd2ba745e84e1a10b08e4b6
maltebp <maltebp94@gmail.com>
2019-01-16 09:10:33 +0100

    Tilfoejet java

--- src/test/java/ChancekortTest.java
+++ src/test/java/ChancekortTest.java
@@ -10,14 +10,24 @@ import java.util.ArrayList;
 public class ChancekortTest {
 
 
+    /**
+     * Test Suite: Chancekort
+     *
+     * Dette er en test suite, der er beregnet til at teste chancekort.
+     * Den indeholder "custom" chancekort, så det er nemmere at se, om
+     * kortene går rigtigt i gennem systemet.
+     * Andet
+     *  - Klassisk braet
+     *  - Falsk raflebaeger
+     *  *
+     * @author Malte
+     */
     public static void main(String[] args) {
 
         Spil spil = SpilGenerator.genererSpil(2);
         spil.setRaflebaeger(new FalskRaflebaeger(2));
 
         Spiller spiller = spil.getSpiller(0);
-
-
 
         ChanceAktionDTO chancekort = new RykkerSpillerCO();
         chancekort.setBeskrivelse("Chancekort 1");
