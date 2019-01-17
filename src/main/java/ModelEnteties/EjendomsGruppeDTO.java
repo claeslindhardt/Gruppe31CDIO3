9662466a75bb8dad7951804a9e92facf668b4a0a
maltebp <maltebp94@gmail.com>
2019-01-17 12:52:36 +0100

    Fjernet ubrugte metoder i ejendomsgrupen

--- src/main/java/ModelEnteties/EjendomsGruppeDTO.java
+++ src/main/java/ModelEnteties/EjendomsGruppeDTO.java
@@ -1,8 +1,6 @@
 package ModelEnteties;
 
 import ModelEnteties.felter.Ejendom;
-
-
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -10,18 +8,10 @@ import java.util.ArrayList;
  * @author Malte
  *  Dette er klassen der holder styr på grupper af ejendomme. En Ejendomsgruppe er et objekt, som identificeres
  *  ved sin farve.
- *  Når man laver et nyt Ejendoms-objekt, sættes den automatisk ind i en gruppe. Der laves en ny gruppe hvis:
- *
- *      1) Det er den første ejendom (og dermed den første gruppe)
- *      2) Hvis gruppen er fuld (der kan maks være GRUPPESTØRRELSE Ejendomme i én gruppe)
- *
  */
-
 public class EjendomsGruppeDTO {
 
-    private int id;
-    private int stoerrelse;
-    private Color farve;
+    private Color   farve;
     private ArrayList<Ejendom> ejendomme = new ArrayList<Ejendom>();
 
     /**
@@ -31,16 +21,9 @@ public class EjendomsGruppeDTO {
      *
      * @param farve Farven som skal kendetegne gruppen (givet kun en visuel sammenligning af gruppen,
      *              og har ingen betydning for hvordan Ejendommene vurderes om de er i samme gruppe.
-     * @param stoerrelse Antallet af Ejendomme som gruppe skal indeholder.
      */
-    public EjendomsGruppeDTO(Color farve, int stoerrelse) {
+    public EjendomsGruppeDTO(Color farve) {
         this.farve = farve;
-        this.stoerrelse = stoerrelse;
-    }
-
-    public EjendomsGruppeDTO( int stoerrelse, int id ){
-        this.id = id;
-
     }
 
     //--------- Getters og Setters:-----------------
@@ -49,63 +32,15 @@ public class EjendomsGruppeDTO {
         return farve;
     }
 
-    /**
-     * @author Malte
-     * Ser om Ejendomsgruppen er fuld (dvs. der ikke bør være flere ejendomme i den).
-     * @return True: den er fuld, False: der er stadig plads.
-     */
-    public boolean erFuld(){
-        return ejendomme.size() >= stoerrelse;
-    }
-
     public ArrayList<Ejendom> getEjendomme() {
         return ejendomme;
     }
 
-
-    public void clearEjendomsListe() {
-        ejendomme.clear();
-    }
-
     public int getAntalEjendomme(){ return ejendomme.size(); }
 
-    /**
-     * @author Malte
-     * Undersoeger om gruppen indeholder en bestemt ejendom.
-     * @param ejendom Ejendommen som gruppen skal indeholder
-     * @return Om den indeholder 'ejendom' eller ej.
-     */
-    public boolean indeholderEjendom(Ejendom ejendom){
-        return ejendomme.contains(ejendom);
-    }
-
-    /** Tilføjer en ejendom til gruppen
-     * @param ejendom Ejendommen der skal tilføjes til gruppen.
-     * @author Malte
-     */
     public void tilfoejEjendom( Ejendom ejendom){
         ejendomme.add(ejendom);
         ejendom.setGruppe(this);
     }
-
-    public void tilfoejEjendomme( Ejendom... ejendomme ){
-        for( Ejendom ejendom : ejendomme ){
-            this.ejendomme.add(ejendom);
-            ejendom.setGruppe(this);
-        }
-    }
-
-
-    //|----------- Metoder:------------------
-
-    //Todo bed malte rykke den her til TUI
-    public void printInfo(){
-        System.out.print("Ejendomsgruppe: "+farve+", har huse: ");
-        //TODO:Implement this.
-        //ejendomme.forEach(a -> System.out.print(a.getNavn()+", "));
-        System.out.println();
-    }
-
-    // TODO: Implementér logikken i denne. Kan være den skal flyttes.
 
 }
