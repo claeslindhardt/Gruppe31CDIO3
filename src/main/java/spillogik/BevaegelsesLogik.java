34ea6509c8530a301ba8a381e8b8d4764c34788a
maltebp <maltebp94@gmail.com>
2019-01-19 20:33:15 +0100

    Fjernet ubrugt metode

--- src/main/java/spillogik/BevaegelsesLogik.java
+++ src/main/java/spillogik/BevaegelsesLogik.java
@@ -10,23 +10,6 @@ public class BevaegelsesLogik {
 
     /**
      * @author Malte
-     * Undersøger om spilleren passerer start, hvis spilleren rykker 'antalFelter'.
-     * Den giver ikke noget output om hvor mange gange man passerer start
-     * (se i stedet {@link #antalGangeOverStart}), eller
-     * hvor meget der udbetales ( se {@link #passererStartPenge(int)}.
-     *
-     * @param startFelt         Spillerens start position
-     * @param antalFelter       Hvor mange felter spilleren rykker frem
-     * @param totalAntalFelter  Hvor mange felter der paa braettet i alt
-     * @return Om spilleren passerer start eller ej.
-     */
-    public static boolean passererSpillerStart( int startFelt, int antalFelter, int totalAntalFelter){
-        return antalGangeOverStart(startFelt, antalFelter, totalAntalFelter) > 0;
-    }
-
-
-    /**
-     * @author Malte
      * Beregner hvor mange gange spilleren passerer start, hvis
      * spilleren rykker 'antalFelter'.
      * Giver ikke noget output om hvor meget man skal have udbetalt
